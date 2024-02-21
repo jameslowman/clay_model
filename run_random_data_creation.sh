2e4bb89f2e1c5af7a4d45ef851dfd52902d53f91
James Lowman <james.robert.lowman@gmail.com>
2024-02-21 11:57:50 -0500

    adding some stuff!

--- run_random_data_creation.sh
+++ run_random_data_creation.sh
@@ -2,11 +2,11 @@
 
 for i in {1..100}; do
     # Generate a random number between 0 and 4000
-    index=$(($RANDOM % 4001))
+    index=$(($RANDOM % 56687))
 
     # Execute your script with the random index
     python scripts/datacube.py \
-        --sample "mgrs_full.fgb"
+        --sample "scipts/mgrs_full.fgb" \
         --bucket "cc-dataocean/clay-model-training-data" \
         --index $index
 done
