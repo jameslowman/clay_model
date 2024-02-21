28ebd7a4cf774e7dcc2570a053617633ac450f24
James Lowman <james.robert.lowman@gmail.com>
2024-02-21 11:48:09 -0500

    adding some stuff!

---
+++ run_random_data_creation.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for i in {1..100}; do
+    # Generate a random number between 0 and 4000
+    index=$(($RANDOM % 4001))
+
+    # Execute your script with the random index
+    python scripts/datacube.py \
+        --bucket "cc-dataocean/clay-model-training-data" \
+        --index $index
+done
